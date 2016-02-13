92bd1b32a378410934040d5bd447e22d9a09f986
LasaleFamine <alessio.occhipinti@gmail.com>
2016-02-13 19:54:19 +0100

    pset1
    Added pset1

---
+++ pset1/greedy.c
@@ -0,0 +1,114 @@
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+
+int getRest(int value, int divider, int rest);
+int division(int value, int divider, int rest);
+
+int main(void)
+{
+    const int coins[4] = {25, 10, 5, 1};
+    int countCoins[4];
+    float val;
+    int converted;
+    bool passOver = false;
+    int rest = -2;
+    int i = 0, countTotal = 0;
+    
+    // Start first loop
+    do
+    {
+        // Ask and get the value
+        printf("Hi, how much change is owed?\n");
+        val = GetFloat();
+        // Check if value is valid
+        if (val <= 0) 
+        {
+            printf("Negative value or not valid\n");
+        }
+        else
+        {
+            // Covert and round the value to cent and int
+            converted = (int) (round(val * 100));   
+            passOver = true;                              
+        }   
+    }
+    while(passOver == false);
+
+    // Initialize the count
+    for(i = 0; i < 4; i++)
+    {   
+        // Make the first division
+        countCoins[i] = division(converted, coins[i], rest); 
+        // Start the count   
+        countTotal += countCoins[i]; 
+                                  
+        if (countCoins[i] != 0)                                 
+        {
+            // Check for remainder
+            rest = getRest(converted, coins[i], rest); 
+        }
+        // If remainder is 0, work done
+        if (rest == 0)                                          
+        {  
+            // Print the total count and exit
+            printf("%d\n", countTotal);            
+            return 0;                                        
+        } 
+              
+    }
+            
+}
+
+
+// Function to make the division
+int division(int value, int divider, int rest)
+{
+    int result;
+
+    if (rest != 0 && rest != -2) 
+    {
+       // If remainder is avail  
+        result = rest / divider;        
+    }
+    else if (rest == -1)
+    {
+        // If rem is not valid
+        return -1;                 
+    }
+    else
+    {
+        // If rem is not avail
+        result = value / divider; 
+    }   
+    
+    return result;
+    
+}
+
+// Function to get the remainder
+int getRest(int value, int divider, int rest)
+{
+    int try;
+    
+    if (rest != 0 && rest != -2) 
+    {
+        try = rest % divider;        
+    }
+    else
+    {
+        try = value % divider;
+    }
+    // If the division have a valid remainder        
+    if (try != divider)     
+    {
+        // Return the value of remainder
+        return try;         
+    }
+    else
+    {   
+        // Return -1 to indicate "impossible to divide"
+        return -1;          
+    } 
+    
+}
---
+++ pset1/mario.c
@@ -0,0 +1,59 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    bool startDraw = false;
+    int i, j;
+    
+    // Start the loop
+    do
+    {
+        // Get int from user
+        printf("Height of pyramid: ");
+        height = GetInt();
+        
+        // Check for valid int
+        if (height < 0)
+        {
+            printf("Negative int is not permitted\n");
+        }
+        else if (height > 23)
+        {
+            printf("Too large\n");
+        }
+        else if (height >= 0 && height <= 23)
+        {
+            // Putting this on "true" state, this loop will finish
+            startDraw = true;
+        }
+        else
+        {
+            // Everything is not in the gap above is not recognized
+            printf("Can't recognize valid int\n");
+        }
+    
+    }
+    while(startDraw == false);
+    // Start drawing
+    for(i = 0; i < height; i++)
+    {
+        for(j = 0; j <= height; j++)
+        {
+            if (j > (height - i - 2))
+            {
+                // If in the right point on the right
+                printf("#");
+            }
+            else 
+            {
+                // Try to get the right point
+                printf(" ");
+            }
+            
+        }
+        // To change row after drawing 
+        printf("\n");        
+    }
+}
